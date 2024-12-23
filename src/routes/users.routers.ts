7466fc413fadc9026c6e18f3cb795e9c29f7a68b
dungnguyen2701 <bvmldung53@gmail.com>
2024-12-23 10:01:20 +0700

    add a register function

--- src/routes/users.routers.ts
+++ src/routes/users.routers.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { loginController } from "~/controllers/users.controllers";
+import { loginController, registerController } from "~/controllers/users.controllers";
 import { loginValidator } from "~/middlewares/users.middlewares";
 const usersRouter = express.Router();
 usersRouter.post("/user", loginValidator, loginController);
+usersRouter.post("/register", registerController)
 export default usersRouter
