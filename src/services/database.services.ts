974f7f8e8ee2777935e90e74517eabe9b8c12f7c
dungnguyen2701 <bvmldung53@gmail.com>
2024-12-23 09:47:09 +0700

    update get user function to get the instance

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,5 +1,6 @@
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import { Collection, Db, MongoClient, ServerApiVersion } from 'mongodb'
 import dotenv from "dotenv";
+import User from '~/models/schemas/User.schema';
 dotenv.config();
 const uri =
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mernstackclut.wnoqo.mongodb.net/?retryWrites=true&w=majority&appName=mernstackClut`
@@ -7,22 +8,29 @@ const uri =
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 class DatabaseService {
   private client: MongoClient
+  private db: Db
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
   async connect() {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       // await client.connect();
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
     } catch (err) {
       console.error(err)
       throw err
     }
   }
+  get user(): Collection<User>{
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
+  }
 }
+
+  
 
 const databaseService = new DatabaseService();
 export default databaseService
