73697c471ce98b9c4736059dc79afb378ef7aaa5
dungnguyen2701 <bvmldung53@gmail.com>
2024-12-23 10:03:00 +0700

    make a registerController

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,8 @@
+import { error } from "console";
 import { Request, Response } from "express";
+import User from "~/models/schemas/User.schema";
+import databaseService from "~/services/database.services";
+import usersServices from "~/services/User.services";
 
 export const loginController = (req: Request, res: Response) => {
   const {email, password} = req.body
@@ -12,6 +16,22 @@ export const loginController = (req: Request, res: Response) => {
   } else{
     res.status(400).json({
       error: "Email or password invalid"
+    })
+  }
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const{email, password} = req.body
+  try{
+    const result = await usersServices.register({email, password})
+    console.log(result);
+    res.json({
+      message: "Register succes"
+    })
+  }catch(err){
+    res.status(400).json({
+      message: "Register failed",
+      error: "err"
     })
   }
 }
